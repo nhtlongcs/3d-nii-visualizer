8c8ea8159a445f2e09d8e17170ef0ba5008a0a7b
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-06-03 17:04:15 +0700

    update color cfg

--- visualizer/config.py
+++ visualizer/config.py
@@ -1,3 +1,4 @@
+import numpy as np
 # default brain settings
 APPLICATION_TITLE = "Theia – NIfTI (nii.gz) 3D Visualizer"
 BRAIN_SMOOTHNESS = 500
@@ -6,13 +7,37 @@ BRAIN_COLORS = [(1.0, 0.9, 0.9)]  # RGB percentages
 
 # default mask settings
 MASK_SMOOTHNESS = 500
-MASK_COLORS = [(1, 0, 0),
-                (0, 1, 0),
-                (1, 1, 0),
-                (0, 0, 1),
-                (1, 0, 1),
-                (0, 1, 1),
-                (1, 0.5, 0.5),
-                (0.5, 1, 0.5),
-                (0.5, 0.5, 1)]  # RGB percentages
+
+
+def color_map(n=256, normalized=False):
+    def bitget(byteval, idx):
+        return (byteval & (1 << idx)) != 0
+
+    dtype = "float32" if normalized else "uint8"
+    cmap = np.zeros((n, 3), dtype=dtype)
+    for i in range(n):
+        r = g = b = 0
+        c = i
+        for j in range(8):
+            r = r | (bitget(c, 0) << 7 - j)
+            g = g | (bitget(c, 1) << 7 - j)
+            b = b | (bitget(c, 2) << 7 - j)
+            c = c >> 3
+
+        cmap[i] = np.array([r, g, b])
+
+    cmap = cmap / 255 if normalized else cmap
+    return cmap
+
+
+MASK_COLORS = color_map(normalized=True)[1:][::-1]
+# [(1, 0, 0),
+#                 (0, 1, 0),
+#                 (1, 1, 0),
+#                 (0, 0, 1),
+#                 (1, 0, 1),
+#                 (0, 1, 1),
+#                 (1, 0.5, 0.5),
+#                 (0.5, 1, 0.5),
+#                 (0.5, 0.5, 1)]  # RGB percentages
 MASK_OPACITY = 1.0
